7ef7028fa87252e03d11e3904657be0aa436d017
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-05-12 06:12:01 +0200

    Comments

--- src/com/dgmf/a/java/basics/main/method/Main.java
+++ src/com/dgmf/a/java/basics/main/method/Main.java
@@ -1,12 +1,38 @@
 package com.dgmf.a.java.basics.main.method;
 
+/**
+ * Javadoc
+ * @author Glaz Dodo
+ */
 public class Main {
+    /**
+     * Javadoc
+     * This Main Method
+     * @param args Command Line Arguments
+     */
     public static void main(String[] args) {
-        // Size ==> 0
+        // Size ==> 0 | Single Line Comment
         System.out.println("Size of 'args' Table is : " + args.length);
+        /*
+        * Multiple Lines Comment
+        * Another Line
+        */
         System.out.println(args[0]);
         System.out.println(args[1]);
-        // ArrayIndexOutOfBoundsException
-        // System.out.println(args[2]);
+        // ArrayIndexOutOfBoundsException | Single Line Comment
+        // System.out.println(args[2]); | Single Line Comment ==> Disable the Line of Code
+
+        System.out.println(sum(15, 2));;
+    }
+
+    /**
+     * Javadoc
+     * Calculate Sum of Integers
+     * @param num1 Operand
+     * @param num2 Operand
+     * @return int
+     */
+    public static int sum(int num1, int num2) {
+        return num1 + num2;
     }
 }
