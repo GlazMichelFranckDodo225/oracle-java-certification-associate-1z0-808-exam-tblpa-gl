410e1cfac93fb43e5aba9b738b30985c10890cae
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-05-19 02:29:07 +0200

    We Can Not Import Classes Outside Default Packages

---
+++ src/com/dgmf/MainBelowComDgmf.java
@@ -0,0 +1,4 @@
+package com.dgmf;
+
+public class MainBelowComDgmf {
+}
--- src/com/dgmf/a/java/basics/namingconflictsandcodeformatting/first/ConflictExample.java
+++ src/com/dgmf/a/java/basics/namingconflictsandcodeformatting/first/ConflictExample.java
@@ -4,11 +4,16 @@ package com.dgmf.a.java.basics.namingconflictsandcodeformatting.first;
 // import java.util.*;
 // import java.sql.*;
 
+import com.dgmf.MainBelowComDgmf;
+
 public class ConflictExample {
     public static void main(String[] args) {
         // Date
         // FQN to Solve the Problem
         java.util.Date date;
         java.sql.Date sqlDate;
+
+        // MainBelowSrc ==> We Can Not Import Classes Outside Default Packages
+        MainBelowComDgmf mainBelowComDgmf = new MainBelowComDgmf();
     }
 }
