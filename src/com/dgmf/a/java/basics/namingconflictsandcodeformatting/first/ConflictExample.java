f0c7d41fd664e1d741ae03515d2b800c66b68bfd
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-05-19 02:38:25 +0200

    Naming Conflicts And Code Formatting

--- src/com/dgmf/a/java/basics/namingconflictsandcodeformatting/first/ConflictExample.java
+++ src/com/dgmf/a/java/basics/namingconflictsandcodeformatting/first/ConflictExample.java
@@ -6,6 +6,8 @@ package com.dgmf.a.java.basics.namingconflictsandcodeformatting.first;
 
 import com.dgmf.MainBelowComDgmf;
 
+import java.util.List;
+
 public class ConflictExample {
     public static void main(String[] args) {
         // Date
@@ -15,5 +17,14 @@ public class ConflictExample {
 
         // MainBelowSrc ==> We Can Not Import Classes Outside Default Packages
         MainBelowComDgmf mainBelowComDgmf = new MainBelowComDgmf();
+
+    }
+
+    public static void checkEmpty(List list) {
+        if (list.isEmpty) {
+            System.out.println("Empty");
+        } else {
+            System.out.println("Not Empty");
+        }
     }
 }
